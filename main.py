4f56286fe6b94ecad78fc42d6531bb70f17b6773
Jakub <jakub.opatowski@gmail.com>
2019-07-05 15:10:03 +0200

    Refactoring

--- main.py
+++ main.py
@@ -1,8 +1,115 @@
 import re
 import os
-import sys
-from PySide2.QtWidgets import QApplication, QLabel
 from enum import Enum
+from io import IOBase
+
+
+class CMakeCreator:
+    def __init__(self, path):
+        assert isinstance(path, str)
+
+        self.path = path
+
+    def __prepare_header(self, file):
+        assert isinstance(file, IOBase)
+
+        file.write('cmake_minimum_required(VERSION 3.1...3.15)\n')
+        file.write('\n')
+        file.write('if(${CMAKE_VERSION} VERSION_LESS 3.12)\n')
+        file.write('    cmake_policy(VERSION ${CMAKE_MAJOR_VERSION}')
+        file.write('.${CMAKE_MINOR_VERSION})\n')
+        file.write('endif()\n')
+        file.write('\n')
+
+    def __prepare_project(self, file, project_name):
+        assert isinstance(file, IOBase)
+        assert isinstance(project_name, str)
+
+        file.write('project(')
+        file.write(project_name)
+        file.write(' VERSION 1.0 LANGUAGES CXX)\n')
+
+    def __add_cxx_flags(self, file, cxx_flags):
+        assert isinstance(file, IOBase)
+        assert isinstance(cxx_flags, str)
+
+        file.write('if(MSVC)\n')
+        file.write('    message(status \"Setting MSVC flags\")\n')
+        file.write('    set(CMAKE_CXX_FLAGS\n')
+        for cxx_flag in cxx_flags:
+            file.write('        ')
+            file.write(cxx_flag)
+            file.write('\n')
+        file.write(')\n')
+        file.write('endif()')
+        file.write('\n')
+
+    def __add_defines(self, file, defines):
+        assert isinstance(file, IOBase)
+        assert isinstance(defines, str)
+
+        file.write('add_definitions(\n')
+        for define in defines:
+            file.write('    -D')
+            file.write(define)
+            file.write('\n')
+        file.write('\n')
+
+    def __export_commands(self, file):
+        assert isinstance(file, IOBase)
+
+        file.write('set(CMAKE_EXPORT_COMPILE_COMMANDS ON)\n\n')
+        file.write('\n')
+
+    def __add_sources(self, file, sources):
+        assert isinstance(file, IOBase)
+        assert isinstance(sources, str)
+
+        file.write('set(project_sources\n')
+        for source in sources:
+            file.write('    \"')
+            file.write(source.replace('\\', '/'))
+            file.write('\"\n')
+        file.write(')\n')
+        file.write('\n')
+
+    def __add_target(self, file, target_type):
+        assert isinstance(file, IOBase)
+        assert isinstance(target_type, ProjectBuildData.TargetType)
+
+        if target_type == ProjectBuildData.TargetType.LIBRARY:
+            file.write('add_library(${PROJECT_NAME}\n')
+            file.write('    ${project_sources})\n')
+        elif target_type == ProjectBuildData.TargetType.EXECUTABLE:
+            file.write('add_executable(${PROJECT_NAME}\n')
+            file.write('    ${project_sources})\n')
+        file.write('\n')
+
+    def __add_includes(self, file, includes):
+        assert isinstance(file, IOBase)
+        assert isinstance(includes, str)
+
+        file.write('target_include_directories(${PROJECT_NAME}\n')
+        file.write('    PRIVATE\n')
+        for include in build_data.list_of_includes:
+            cmake.write('    \"')
+            cmake.write(include.replace('\\', '/'))
+            cmake.write('\"\n')
+
+        cmake.write(')\n')
+
+    def create_project(self, build_data):
+        assert isinstance(build_data, ProjectBuildData)
+
+        file = open(os.path.join(self.path, 'CMakeLists.txt'), 'w')
+        self.__prepare_header(file)
+        self.__prepare_project(file, build_data.project_name)
+        self.__add_cxx_flags(file, build_data.list_of_flags)
+        self.__add_defines(file, build_data.list_of_defines)
+        self.__export_commands(file)
+        self.__add_target(file, build_data.target_type)
+
+        file.close()
 
 
 class ProjectBuildData:
@@ -73,6 +180,7 @@ class MakefileParser:
         assert isinstance(verbose, bool)
         self.project_path = project_path
         self.verbose = verbose
+        self.subprojects = []
 
     def transform_project(self):
         self.list_of_subprojects = self.get_subprojects()
@@ -85,16 +193,22 @@ class MakefileParser:
 
             self.parse_file(makefile_path)
 
-    def make_main_cmake():
+    def make_main_cmake(self):
         print(project_path)
+        file = open(os.path.join(self.project_path, 'CMakeLists.txt'), 'w')
+        file.write()
 
     def get_subprojects(self):
-        return [name for name in os.listdir(self.project_path)
-                if os.path.isdir(os.path.join(self.project_path, name))]
+        path = os.path.join('c:\\', 'Projekty', 'trunk',
+                            'win32-msvc2015_d')
+        return [name for name in os.listdir(path)
+                if os.path.isfile(os.path.join(path, name, 'qt4',
+                                               'makefile'))]
 
     def parse_file(self, makefile_path, project_path):
         assert isinstance(makefile_path, str)
 
+        self.subprojects.append(project_path)
         if os.path.isfile(makefile_path):
             with open(makefile_path) as makefile:
                 makefile_data = makefile.read()
@@ -161,53 +275,10 @@ class MakefileParser:
         assert isinstance(build_data, ProjectBuildData)
 
         print('Creating CMakeLists.txt')
-        cmake_path = os.path.join(build_data.project_path, 'CMakeLists.txt')
-        cmake = open(cmake_path, 'w')
-        cmake.write('cmake_minimum_required(VERSION 3.1...3.15)\n')
-        cmake.write('\n')
-        cmake.write('if(${CMAKE_VERSION} VERSION_LESS 3.12)\n')
-        cmake.write('    cmake_policy(VERSION ${CMAKE_MAJOR_VERSION}')
-        cmake.write('.${CMAKE_MINOR_VERSION})\n')
-        cmake.write('endif()\n')
-        cmake.write('\n')
-        cmake.write('if(MSVC)\n')
-        cmake.write('    message(status \"Setting MSVC flags\")\n')
-        cmake.write('    set(CMAKE_CXX_FLAGS')
-        for cxx_flag in build_data.list_of_flags:
-            cmake.write(' ')
-            cmake.write(cxx_flag)
-        for define in build_data.list_of_defines:
-            cmake.write(' ')
-            cmake.write('-D')
-            cmake.write(define)
-
-        cmake.write(')\n')
-        cmake.write('endif()\n')
-        cmake.write('set(CMAKE_EXPORT_COMPILE_COMMANDS ON)\n')
-        cmake.write('message(status "** CMAKE_CXX_FLAGS: ')
-        cmake.write('${CMAKE_CXX_FLAGS}")\n')
-        cmake.write('set(project_sources\n')
-        for source in build_data.list_of_sources:
-            cmake.write('    \"')
-            cmake.write(source.replace('\\', '/'))
-            cmake.write('\"\n')
-
-        cmake.write(')\n')
-        if build_data.target_type == build_data.TargetType.LIBRARY:
-            cmake.write('add_library(${PROJECT_NAME}\n')
-            cmake.write('    ${project_sources})\n')
-        elif build_data.target_type == build_data.TargetType.EXECUTABLE:
-            cmake.write('add_executable(${PROJECT_NAME}\n')
-            cmake.write('    ${project_sources})\n')
-
-        cmake.write('target_include_directories(${PROJECT_NAME}\n')
-        cmake.write('    PRIVATE\n')
-        for include in build_data.list_of_includes:
-            cmake.write('    \"')
-            cmake.write(include.replace('\\', '/'))
-            cmake.write('\"\n')
-
-        cmake.write(')\n')
+        cmake = CMakeCreator(build_data.project_path)
+        cmake.create_project(build_data)
+        
+ 
 
 
 project_path = os.path.join('c:\\', 'Projekty', 'trunk', 'src')
@@ -224,9 +295,3 @@ for subproject in subprojects_list:
 
     build_data = makefile_parser.parse_file(makefile_path, subproject_path)
     makefile_parser.make_cmake(build_data)
-# print(makefile_parser.list_of_subprojects())
-
-# app = QApplication(sys.argv)
-# label = QLabel("Hello World!")
-# label.show()
-# sys.exit(app.exec_())
